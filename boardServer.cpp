4b89862694682aa645f966331cd152b81e898e98
zhou0745 <zhou0745@umn.edu>
2018-03-20 23:04:25 -0500

    changed the position for RegisterThisServer

--- boardServer.cpp
+++ boardServer.cpp
@@ -8,6 +8,14 @@ string coordinator_server_addr = "";
 int coordinator_server_port = 0;
 int mode_num = 0;
 
+// this function will return the implement the coordinator server took
+// if -1, means connecting met error
+int RegisterThisServer(void) {
+  string message_send = "RR";
+  return -1;
+}
+
+
 int main() {
   // get local IP address in string form via self-defined API
   local_ip_addr = get_local_IP();
@@ -60,13 +68,7 @@ int main() {
     printf("Please enter the port number for the coordinator server: ");
     cin >> coordinator_server_port;
     printf("This server will be initialized as a regular server\n");
-    mode_num = RegisterThisServer(
-                 coordinator_server_addr,
-                 coordinator_server_port,
-                 local_ip_addr,
-                 local_port_num,
-                 local_socket_fd
-               );
+    mode_num = RegisterThisServer();
     if (mode_num == -1) {
       printf("Failed to register this server to the coordinator\n");
       printf("Exiting...\n");
